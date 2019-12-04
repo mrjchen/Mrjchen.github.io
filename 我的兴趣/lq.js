b8475498134cce9fbd37169125771b46201e49d0
mrjchen <54805015+mrjchen@users.noreply.github.com>
2019-12-04 14:43:12 +0800

    Add files via upload

---
+++ 我的兴趣/lq.js
@@ -0,0 +1,20 @@
+$(function(){
+	$("#spxq").hide();
+	$("#sp").hover(function(){
+		$("#spxq").show(500);
+	},function(){
+		$("#spxq").hide(300);
+	})
+	$(".qy").hide();
+	$("#qy").hover(function(){
+		$(".qy").show(500);
+	},function(){
+		$(".qy").hide(300);
+	})
+	$(".qd").hide();
+	$("#qd").hover(function(){
+		$(".qd").show(500);
+	},function(){
+		$(".qd").hide(300);
+	})
+})
